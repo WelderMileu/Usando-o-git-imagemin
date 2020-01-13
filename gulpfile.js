b763a2ac2ee6c855d49f40dd5b3c726023de9447
WelderMileu <welderfelix21@gmail.com>
2020-01-13 16:18:21 -0200

    funcao finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 
-exports.default = () => (
-	gulp.src("img/**/*")
-		.pipe(imagemin())
-		.pipe(gulp.dest("img/"))
-);
+exports.default = async () => {
+	await gulp.src('img/**/*')
+			  .pipe(imagemin())
+			  .pipe(gulp.dest('build/img/'))
+};
